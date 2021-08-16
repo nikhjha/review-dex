790f0bb575aa3250fe765a96590407c613bc342a
nikhjha <nikh.jha550@gmail.com>
2021-08-17 01:16:31 +0530

    Correcting api awiat

--- server/api.js
+++ server/api.js
@@ -2,72 +2,59 @@ import Router from "koa-router";
 import Shopify from "@shopify/shopify-api";
 import Merchant from "./model/merchant";
 import Review from "./model/review";
+import { canNotDefineSchemaWithinExtensionMessage } from "graphql/validation/rules/LoneSchemaDefinition";
 
 const router =  Router();
 
-router.get("/getMerchantDetail/:id",(ctx)=>{
+router.get("/getMerchantDetail/:id",async(ctx)=>{
     const shop = ctx.params.id;
-    Merchant.findOne({shop : shop}, (err,doc) => {
-        if(err){
-            ctx.response.status = 400;
-            ctx.response.body = err;
-        }
-        else if(doc.totalReviews === 0){
-            ctx.response.status = 200;
-            ctx.response.body = {merchant : doc , reviews : []};
-        }
-        else{
-            Review.find({merchantID : doc.id, hidden : false},null,{limit : 6}, (err,reviews) => {
-                if(err){
-                    ctx.response.status = 400;
-                    ctx.response.body = err;
-                    
-                }
-                else{
-                    ctx.response.status = 200;
-                    ctx.response.body = {merchant : doc , reviews};
-                }
-            });
-        }
-    });
+    try {
+        const doc = await Merchant.findOne({shop : shop});
+    if(doc.totalReviews === 0){
+        ctx.response.status = 200;
+        ctx.response.body = {merchant : doc , reviews : []};
+        return;
+    }
+    const reviews = await Review.find({merchantID : doc.id, hidden : false},null,{limit : 6}); 
+    ctx.response.status = 200;
+    ctx.response.body = {merchant : doc , reviews}; 
+    }
+    catch(e){
+        ctx.response.status = 400;
+    ctx.response.body = e; 
+    }
 });
 
 router.get("/reviews", async(ctx) => {
     const {shop} = await Shopify.Utils.loadCurrentSession(ctx.req, ctx.res);
-    Merchant.findOne({shop : shop}, (err,doc) => {
-        if(err){
-            ctx.response.status = 400;
-            ctx.response.body = err;
-        }
-        else if(doc.totalReviews === 0){
-            ctx.response.status = 200;
-            ctx.response.body = { reviews : []};
-        }
-        else{
-            Review.find({merchantID : doc.id},null,null, (err,reviews) => {
-                if(err){
-                    ctx.response.status = 400;
-                    ctx.response.body = err;
-                }else{
-                    ctx.response.status = 200;
-                    ctx.response.body = {reviews};
-                }
-            });
-        }
-    });
+    try {
+        const doc = await Merchant.findOne({shop : shop});
+    if(doc.totalReviews === 0){
+        ctx.response.status = 200;
+        ctx.response.body = { reviews : []};
+        return;
+    }
+    const reviews = await Review.find({merchantID : doc.id, hidden : false},null,{limit : 6}); 
+    ctx.response.status = 200;
+    ctx.response.body = { reviews}; 
+    }
+    catch(e){
+        ctx.response.status = 400;
+    ctx.response.body = e; 
+    }
 });
 
 router.get("/merchant", async(ctx) => {
     const {shop} = await Shopify.Utils.loadCurrentSession(ctx.req, ctx.res);
-    Merchant.findOne({shop : shop}, (err,doc) => {
-        if(err){
-            ctx.response.status = 400;
-            ctx.response.body = err;
-        }else{
-            ctx.response.status = 200;
-            ctx.response.body = {merchant : docs};
-        }
-    });
+    try {
+        const doc = await Merchant.findOne({shop : shop});
+        ctx.response.status = 200;
+    ctx.response.body = { merchant : doc}; 
+    }
+    catch(e){
+        ctx.response.status = 400;
+    ctx.response.body = e; 
+    }
 });
 
 export default router;
