978eaf7a5891d60ef62e211bdd0e25a77a8e13c4
nikhjha <nikh.jha550@gmail.com>
2021-08-27 21:02:22 +0530

    Server

--- template_shopify/review_dex.js
+++ template_shopify/review_dex.js
@@ -379,7 +379,6 @@ class ReviewPanel {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
       e.preventDefault();
-
       document.querySelector(this.getAttribute('href')).scrollIntoView({
           behavior: 'smooth'
       });
